0f2e7b78723f8b93f4d88491d582eebaaecdaebb
Programmer Ammo <100178397+EternalBlue3@users.noreply.github.com>
2023-05-12 17:05:20 -0400

    Update Morse_Code.py

--- Morse_Code.py
+++ Morse_Code.py
@@ -1,122 +1,42 @@
 import pygame, time
 pygame.init()
 
-char_to_morse = {
-'!':"-.-.--",
-'"':".-..-.",
-'$':"...-..-",
-'&':".-...",
-"'":".----.",
-'(':"-.--.",
-')':"-.--.-",
-'+':".-.-.",
-',':"--..--",
-'-':"-....-",
-'.':".-.-.-",
-'/':"-..-.",
-'0':"-----",
-'1':".----",
-'2':"..---",
-'3':"...--",
-'4':"....-",
-'5':".....",
-'6':"-....",
-'7':"--...",
-'8':"---..",
-'9':"----.",
-':':"---...",
-';':"-.-.-.",
-'=':"-...-",
-'?':"..--..",
-'@':".--.-.",
-'_':"..--.-",
-'A':".-",
-'B':"-...",
-'C':"-.-.",
-'D':"-..",
-'E':".",
-'F':"..-.",
-'G':"--.",
-'H':"....",
-'I':"..",
-'J':".---",
-'K':"-.-",
-'L':".-..",
-'M':"--",
-'N':"-.",
-'O':"---",
-'P':".--.",
-'Q':"--.-",
-'R':".-.",
-'S':"...",
-'T':"-",
-'U':"..-",
-'V':"...-",
-'W':".--",
-'X':"-..-",
-'Y':"-.--",
-'Z':"--.."
-}
+char_to_morse = {'!':"-.-.--",'"':".-..-.",'$':"...-..-",'&':".-...","'":".----.",'(':"-.--.",')':"-.--.-",'+':".-.-.",',':"--..--",'-':"-....-",'.':".-.-.-",'/':"-..-.",'0':"-----",'1':".----",'2':"..---",'3':"...--",'4':"....-",'5':".....",'6':"-....",'7':"--...",'8':"---..",'9':"----.",':':"---...",';':"-.-.-.",'=':"-...-",'?':"..--..",'@':".--.-.",'_':"..--.-",'A':".-",'B':"-...",'C':"-.-.",'D':"-..",'E':".",'F':"..-.",'G':"--.",'H':"....",'I':"..",'J':".---",'K':"-.-",'L':".-..",'M':"--",'N':"-.",'O':"---",'P':".--.",'Q':"--.-",'R':".-.",'S':"...",'T':"-",'U':"..-",'V':"...-",'W':".--",'X':"-..-",'Y':"-.--",'Z':"--.."}
 morse_to_char = {v:k for k,v in char_to_morse.items()}
 
-char_delimit = " "
-word_delimit = "  "
-# Between chars is 2 spaces
-def charToMorse(c):
-    if c in char_to_morse:
-        return char_to_morse[c]
-    elif c == ' ':
-        return char_delimit
-    else:
-        return ""
-
-# Between words is 3 spaces
-def writeMorse(msg):
-    out = []
-    for word in msg.upper().split(" "):
-        out.append(char_delimit.join( filter(bool, map(charToMorse, word)) ))
-    return word_delimit.join(out)
-
-def morseToChar(m):
-    if m in morse_to_char:
-        return morse_to_char[m]
-    else:
-        return ""
-
-def readMorse(morse):
-    out = []
-    for word in morse.split(word_delimit):
-        out.append( "".join( filter(bool, map(morseToChar, word.split(char_delimit))) ) )
+def text_to_morse(msg):
+    out = [char_to_morse[x] if x in char_to_morse else '' for x in msg.upper()]
     return " ".join(out)
 
+def morse_to_text(morse):
+    return " ".join("".join(morse_to_char.get(code, "") for code in word.split(" ")) for word in morse.split("  "))
+
 def play_morse_sound(morse):
+    dot = pygame.mixer.Sound("dot.wav")
+    dash = pygame.mixer.Sound("dash.wav")
     
-    for x in range(len(morse)):
-        if morse[x] != ' ':
-            if morse[x] == '.':
-                dot = pygame.mixer.Sound("dot.wav")
+    for x in morse:
+        if x != ' ':
+            if x == '.':
                 dot.play()
-                pygame.time.wait(int(dot.get_length() * 1400))
+                pygame.time.wait(200)
             else:
-                dash = pygame.mixer.Sound("dash.wav")
                 dash.play()
-                pygame.time.wait(int(dash.get_length() * 1200))
+                pygame.time.wait(400)
         else:
-            time.sleep(0.2)
-            
-    time.sleep(0.2)
+            pygame.time.wait(200)
+    pygame.time.wait(200)
 
-def doTest(test_message):
+def main(message):
     print("--------------------------------------------------------------")
-    print("Original: %s" % test_message.upper())
-    a = writeMorse(test_message)
-    print("Morse: %s" % a)
-    b = readMorse(a)
-    print("and Back: %s" % readMorse(a))
-    print("Valid? %s" % ("Yes" if test_message.upper() == b else "No"))
+    print(f"Original: {message.upper()}")
+    encoded = text_to_morse(message)
+    print(f"Encoded: {encoded}")
+    print(f"Decoded: {morse_to_text(encoded)}")
     print("--------------------------------------------------------------")
-    return a
+    
+    play_morse_sound(encoded)
 
-a = doTest(str(input("Enter message: ")))
-
-play_morse_sound(a)
+if __name__ == '__main__':
+    message = input("Enter message: ")
+    main(message)
